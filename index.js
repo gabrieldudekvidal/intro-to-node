1e728f98dcd2f596403106088bdb28c01d3c8832
Gabriel Dudek Vidal <gabrieldkvl@icloud.com>
2022-03-24 17:05:21 -0300

    NPM package

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var superheores = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperHero = superheores.random();
+var mySuperVillain = supervillains.random();
+
+console.log(mySuperHero + " vs " + mySuperVillain);
